1258a9ea4907f05588e09a9cdd27b2beb91702bd
Saliou Sarr <saliou.sarr7800@gmail.com>
2026-04-16 00:11:09 +0000

    Ajout app authen

---
+++ authen/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+# Create your views here.
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
--- first_project/urls.py
+++ first_project/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from first_project.view import index, login, register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('login/', login, name='login'),
-    path('register/', register, name='register')
+    path('', include('authen.urls')),
+
 ]
+
