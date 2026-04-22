ddca704cc4b6fdef7e4a7486f9fd558884f998d6
= <tenedouglas237@gmail.com>
2026-04-22 20:03:47 +0100

    recuperation du code

--- first_project/settings.py
+++ first_project/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'PASSWORD': config('DB_PASSWORD'), 
         'HOST':config('DB_HOST'),
         'PORT': config('DB_PORT'),
     }
--- products/forms.py
+++ products/forms.py
@@ -1,8 +1,24 @@
 from django import forms
 from .models import Product
 
-class ProductForm(forms.ModelForm):
+# class ProductForm(forms.ModelForm):
     
-    class Meta:
-        model = Product
-        fields = ['libele', 'description', 'prix', 'image','stock']
+#     class Meta:
+#         model = Product
+#         fields = ['libele', 'description', 'prix', 'image','stock']
+class ProductForm(forms.Form):
+    libele = forms.CharField(required=True, label="Entrer le libele", max_length=30, min_length=1, widget=forms.TextInput(attrs={
+        'class':"libele",
+        'id':'libele',
+        'name':'libele',
+    }))
+    description = forms.CharField(required=True, label="Entrer la description", max_length=30, min_length=1, widget=forms.TextInput(attrs={
+        'class':"description",
+        'id':'description',
+        'name':'description',
+    }))
+    prix = forms.DecimalField(required=True, label="Entrer la description", widget=forms.NumberInput(attrs={
+        'class':"prix",
+        'id':'prix',
+        'name':'prix',
+    }))
