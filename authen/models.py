69cb6c1e536b51381e99e19fe5f80e28257ff13a
= <tenedouglas237@gmail.com>
2026-05-15 20:51:19 +0100

    role et permission cote code

--- authen/models.py
+++ authen/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
+user_in_migrattion = True
 # Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('clients', 'Client'), 
+        ('sellers', 'Seller')
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='clients'
+    )
--- authen/signals.py
+++ authen/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_migrate
+from django.db.models.signals import post_migrate, post_save
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
@@ -26,3 +26,16 @@ def create_roles_and_permissions(sender, **kwargs):
 
     clients_group.permissions.add(can_view_all_product)
     # admin_group.permissions.set([can_view_all_product, creat_product,])
+
+
+@receiver(post_save, sender=User)
+def assign_user_to_groupe(sender, instance, created, **kwargs):
+    if created:
+        if instance.role == 'sellers':
+            seller_group, _ = Group.objects.get_or_create(name='sellers')
+            instance.groups.add(seller_group)
+            instance.save()
+        elif instance.role == "clients":
+            clients_group, _ = Group.objects.get_or_create(name='clients')
+            instance.groups.add(clients_group)
+            instance.save()
